419db0123a7f6bec7caf6ffbe3486fe418808b99
Yusuke Mizuno <poke4202015@gmail.com>
2021-11-19 01:32:39 +0900

    for heroku

--- voiceentry.py
+++ voiceentry.py
@@ -1,8 +1,6 @@
 import discord
 
 client = discord.Client()
-TOKEN = "THIS_IS_EXMAPLE"
-TEXT_CHANNEL = 123456789
 
 # チャンネル入退室時の通知処理
 @client.event
@@ -10,11 +8,11 @@ async def on_voice_state_update(member, before, after):
 
     # チャンネルへの入室ステータスが変更されたとき（ミュートON、OFFに反応しないように分岐）
         # 通知メッセージを書き込むテキストチャンネル（チャンネルIDを指定）
-        botRoom = client.get_channel(TEXT_CHANNEL)
+        botRoom = client.get_channel(TEXT_ID)
 
         # 入室通知（画面共有に反応しないように分岐）
         if after.channel is not None and after.channel is not before.channel:
             await botRoom.send("**" + after.channel.name + "** に、__" + member.name + "__  が参加しました！")
 
 # Botのトークンを指定（デベロッパーサイトで確認可能）
-client.run(TOKEN)
+client.run(DISCORD_TOKEN)
